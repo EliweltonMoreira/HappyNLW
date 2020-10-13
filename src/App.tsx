c60071663e7c6165c2ab8ec9e8a3baaff5ea2563
EliweltonMoreira <eliwelton.moreira@outlook.com>
2020-10-12 23:26:26 -0300

    Add first style and import into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-
-interface TitleProps {
-  text: string;
-}
-
-function Title(props: TitleProps) {
-  return <h1>{props.text}</h1>;
-}
+import "./styles/global.css";
 
 function App() {
   return (
     <div className="App">
-      <Title text="Title 1" />
-      <Title text="Title 2" />
-      <Title text="Title 3" />
-      <Title text="Title 4" />
+      <h1>Hello World</h1>
     </div>
   );
 }
