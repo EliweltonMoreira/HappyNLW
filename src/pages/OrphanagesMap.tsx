9c8808803b88c2196d16d5e152c03f5d71e37b52
EliweltonMoreira <eliwelton.moreira@outlook.com>
2020-10-14 09:42:25 -0300

    Add more two pages and your stylesheets

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -48,14 +48,14 @@ function OrphanagesMap() {
             className="map-popup"
           >
             Girls home
-            <Link to="">
+            <Link to="/orphanages/1">
               <FiArrowRight size={20} color="#fff" />
             </Link>
           </Popup>
         </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
